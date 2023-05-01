c11cd4d850ca8d0f123bec28271f27e68c3091a9
newt239 <ysti2019.win2020.st@gmail.com>
2023-05-02 00:31:21 +0900

    add: setup prisma

--- app/root.tsx
+++ app/root.tsx
@@ -21,28 +21,6 @@ export default function App() {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
-          rel="stylesheet"
-        />
-        <script src="https://unpkg.com/alpinejs"></script>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/tippy.js/6.3.7/tippy.min.css"
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
-        <script
-          src="https://unpkg.com/@ryangjchandler/alpine-tooltip@1.2.0/dist/cdn.min.js"
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        ></script>
       </head>
       <body>
         <Outlet />
