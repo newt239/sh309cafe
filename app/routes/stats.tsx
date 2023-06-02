dc77ae1562e8c8d4975d7c8ec7e39d24e545c6b0
newt239 <ysti2019.win2020.st@gmail.com>
2023-06-02 19:49:18 +0900

    add: pie chart

--- app/routes/stats.tsx
+++ app/routes/stats.tsx
@@ -4,6 +4,7 @@ import { Form, Outlet, useLoaderData } from "@remix-run/react";
 
 import clsx from "clsx";
 import dayjs from "dayjs";
+import { Cell, Legend, Pie, PieChart, ResponsiveContainer } from "recharts";
 
 import H2 from "@/components/common/H2";
 import H3 from "@/components/common/H3";
@@ -17,6 +18,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/Table";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/Tabs";
 import prisma from "@/lib/prisma";
 import { cn } from "@/lib/utils";
 
@@ -67,72 +69,130 @@ export async function action({ request }: ActionArgs) {
 }
 
 export default function Stats() {
+  const COLORS = [
+    "hsl(173 83.4% 32.5%)",
+    "hsl(167 59.3% 63.1%)",
+    "hsl(152 57.5% 37.6%)",
+    "hsl(80 68.3% 46.9%)",
+  ];
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+  }: {
+    [key: string]: number;
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text
+        dominantBaseline="central"
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        x={x}
+        y={y}
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
   const { menus, guests, orders } = useLoaderData<typeof loader>();
+
+  const pie = orders.map((order) => {
+    const menu = menus.find((menu) => menu.id === order.menu_id);
+    return { name: menu?.name, value: order._sum.count };
+  });
 
   return (
     <ScrollArea className={clsx("p-3", "grow", "h-screen")}>
       <H2>統計</H2>
-      <div className={cn("flex", "gap-3")}>
-        <div className={cn("grow")}>
+      <Tabs className={cn("w-full", "pt-3")} defaultValue="guests">
+        <TabsList>
+          <TabsTrigger value="guests">ゲスト一覧</TabsTrigger>
+          <TabsTrigger value="orders">オーダー一覧</TabsTrigger>
+          <TabsTrigger value="pie">売上比</TabsTrigger>
+        </TabsList>
+        <TabsContent value="guests">
           <H3>ゲスト一覧</H3>
-          <ScrollArea className={cn("grow", "overflow-x-auto", "p-3")}>
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead>番号札</TableHead>
-                  <TableHead>人数</TableHead>
-                  <TableHead>入店時刻</TableHead>
-                  <TableHead>滞在時間</TableHead>
-                  <TableHead></TableHead>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>番号札</TableHead>
+                <TableHead>人数</TableHead>
+                <TableHead>入店時刻</TableHead>
+                <TableHead>滞在時間</TableHead>
+                <TableHead></TableHead>
+              </TableRow>
+            </TableHeader>
+            <tbody>
+              {guests.map((guest) => (
+                <TableRow key={guest.id}>
+                  <TableCell>{guest.card_number}</TableCell>
+                  <TableCell>{guest.count}</TableCell>
+                  <TableCell>
+                    {dayjs(guest.enter_at).format("HH:mm:ss")}
+                  </TableCell>
+                  <TableCell>
+                    {dayjs(guest.exit_at).diff(guest.enter_at, "minutes")}分
+                  </TableCell>
+                  <TableCell>
+                    <Form method="post">
+                      <Button
+                        name="action"
+                        value={guest.id}
+                        variant="destructive"
+                      >
+                        取り消す
+                      </Button>
+                    </Form>
+                  </TableCell>
                 </TableRow>
-              </TableHeader>
-              <tbody>
-                {guests.map((guest) => (
-                  <TableRow key={guest.id}>
-                    <TableCell>{guest.card_number}</TableCell>
-                    <TableCell>{guest.count}</TableCell>
-                    <TableCell>
-                      {dayjs(guest.enter_at).format("HH:mm:ss")}
-                    </TableCell>
-                    <TableCell>
-                      {dayjs(guest.exit_at).diff(guest.enter_at, "minutes")}分
-                    </TableCell>
-                    <TableCell>
-                      <Form method="post">
-                        <Button
-                          name="action"
-                          value={guest.id}
-                          variant="destructive"
-                        >
-                          取り消す
-                        </Button>
-                      </Form>
-                    </TableCell>
-                  </TableRow>
+              ))}
+              {guests.length === 0 && (
+                <TableRow>
+                  <TableCell colSpan={4}>
+                    <Alert>現在、店内にはゲストがいません。</Alert>
+                  </TableCell>
+                </TableRow>
+              )}
+            </tbody>
+          </Table>
+        </TabsContent>
+        <TabsContent value="orders">
+          <H3>オーダー一覧</H3>
+        </TabsContent>
+        <TabsContent value="pie">
+          <H3>売上比</H3>
+          <ResponsiveContainer height={500} width="100%">
+            <PieChart>
+              <Legend layout="vertical" />
+              <Pie
+                cx="50%"
+                cy="50%"
+                data={pie}
+                dataKey="value"
+                fill="#8884d8"
+                label={renderCustomizedLabel}
+              >
+                {pie.map((menu, index) => (
+                  <Cell
+                    fill={COLORS[index % COLORS.length]}
+                    key={`cell-${index}`}
+                  />
                 ))}
-                {guests.length === 0 && (
-                  <TableRow>
-                    <TableCell colSpan={4}>
-                      <Alert>現在、店内にはゲストがいません。</Alert>
-                    </TableCell>
-                  </TableRow>
-                )}
-              </tbody>
-            </Table>
-          </ScrollArea>
-        </div>
-        <div className={cn("min-w-[300px]")}>
-          <H3>売上</H3>
-          {orders.map((order) => {
-            const menu = menus.find((menu) => menu.id === order.menu_id);
-            return (
-              <div key={order.menu_id}>
-                {menu?.name} {order._sum.count} / {menu?.stock}
-              </div>
-            );
-          })}
-        </div>
-      </div>
+              </Pie>
+            </PieChart>
+          </ResponsiveContainer>
+        </TabsContent>
+      </Tabs>
       <Outlet />
     </ScrollArea>
   );
