517ce5d464f419e3abeaa549395e63d8c1eb4922
newt239 <ysti2019.win2020.st@gmail.com>
2023-06-04 11:51:17 +0900

    add: form validation

--- app/routes/console.enter.tsx
+++ app/routes/console.enter.tsx
@@ -1,9 +1,14 @@
 import type { ActionArgs, V2_MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Form, useLoaderData, useTransition } from "@remix-run/react";
-import { useEffect, useRef } from "react";
+import {
+  Form,
+  useActionData,
+  useLoaderData,
+  useTransition,
+} from "@remix-run/react";
+import { useEffect, useRef, useState } from "react";
 
-import { CupSoda } from "lucide-react";
+import { CupSoda, Minus, Plus } from "lucide-react";
 import { nanoid } from "nanoid";
 
 import OrderInput from "@/components/feature/enter/OrderInput";
@@ -28,6 +33,19 @@ export async function action({ request }: ActionArgs) {
   const formData = await request.formData();
   const guestCount = formData.get("guest-count");
   const cardNumber = formData.get("card-number");
+
+  const guests = await prisma.guest.findMany({
+    where: { exit_at: null, available: 1 },
+  });
+  const cardNumberList = guests.map((guest) => guest.card_number);
+
+  if (cardNumberList.includes(Number(cardNumber))) {
+    return {
+      status: "error",
+      id: "card-number",
+      message: `番号札${cardNumber}は使用中です。`,
+    };
+  }
 
   const datetime = new Date();
   const guestId = nanoid(10);
@@ -60,23 +78,27 @@ export async function action({ request }: ActionArgs) {
     await prisma.order.createMany({
       data: orders,
     });
-    return { message: "success" };
+    return { status: "success" };
   } else {
-    return { message: "error" };
+    return { status: "error" };
   }
 }
 
 export default function Enter() {
   const data = useLoaderData<typeof loader>();
   const transition = useTransition();
+  const actionData = useActionData();
   const formRef = useRef<HTMLFormElement>(null);
+
+  const [guestCount, setGuestCount] = useState(4);
+  const [cardNumber, setCardNumber] = useState(1);
 
   const isAdding =
     transition.submission &&
     transition.submission.formData.get("action") === "create";
 
   useEffect(() => {
-    if (!isAdding) {
+    if (!isAdding && actionData?.status === "success") {
       formRef.current?.reset();
     }
   }, [isAdding]);
@@ -90,26 +112,80 @@ export default function Enter() {
         <Form method="post" ref={formRef}>
           <div className={cn("pb-3")}>
             <Label htmlFor="guest-count">人数</Label>
-            <Input
-              defaultValue={4}
-              id="guest-count"
-              max={4}
-              min={0}
-              name="guest-count"
-              type="number"
-            />
+            <div className={cn("flex", "gap-3", "pb-3")}>
+              <Button
+                disabled={guestCount === 1}
+                onClick={() => {
+                  setGuestCount((v) => v - 1);
+                }}
+                type="button"
+                variant="secondary"
+              >
+                <Minus />
+              </Button>
+              <Input
+                id="guest-count"
+                max={4}
+                min={1}
+                name="guest-count"
+                onChange={(e) => {
+                  setGuestCount(Number(e.target.value));
+                }}
+                type="number"
+                value={guestCount}
+              />
+              <Button
+                disabled={guestCount === 4}
+                onClick={() => {
+                  setGuestCount((v) => v + 1);
+                }}
+                type="button"
+                variant="secondary"
+              >
+                <Plus />
+              </Button>
+            </div>
           </div>
           <div className={cn("pb-3")}>
             <Label htmlFor="card-numer">番号札</Label>
-            <Input
-              defaultValue={1}
-              id="card-numer"
-              max={30}
-              min={1}
-              name="card-number"
-              type="number"
-            />
+            <div className={cn("flex", "gap-3", "pb-3")}>
+              <Button
+                disabled={cardNumber === 1}
+                onClick={() => {
+                  setCardNumber((v) => v - 1);
+                }}
+                type="button"
+                variant="secondary"
+              >
+                <Minus />
+              </Button>
+              <Input
+                id="card-numer"
+                max={50}
+                min={1}
+                name="card-number"
+                onChange={(e) => {
+                  setCardNumber(Number(e.target.value));
+                }}
+                type="number"
+                value={cardNumber}
+              />
+              <Button
+                disabled={cardNumber === 50}
+                onClick={() => {
+                  setCardNumber((v) => v + 1);
+                }}
+                type="button"
+                variant="secondary"
+              >
+                <Plus />
+              </Button>
+            </div>
+            {actionData?.message && actionData?.id === "card-number" && (
+              <p className="text-red-400">{actionData.message}</p>
+            )}
           </div>
+
           <OrderInput menus={data.menus} refresh={isAdding} />
 
           <Button
