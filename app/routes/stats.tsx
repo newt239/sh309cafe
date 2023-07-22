939e4ae54bd5478bce35c1537606c406b5c31ea6
newt239 <ysti2019.win2020.st@gmail.com>
2023-07-23 00:13:17 +0900

    add: sum order card

--- app/routes/stats.tsx
+++ app/routes/stats.tsx
@@ -1,6 +1,8 @@
 import { json, type V2_MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
+import { Card } from "@tremor/react";
+
 import Hourly from "@/components/feature/stats/Bar";
 import Pie from "@/components/feature/stats/Pie";
 import { ScrollArea } from "@/components/ui/ScrollArea";
@@ -18,12 +20,20 @@ export async function loader() {
     _sum: { count: true },
   });
   const hourlyOrderCounts = await getHourlyOrderCounts();
+  const sumOrderCount = orders.reduce((acc, order) => {
+    return acc + (order._sum.count || 0);
+  }, 0);
+  const sumFee = orders.reduce((acc, order) => {
+    const menu = menuList.find((menu) => menu.id === order.menu_id);
+    return acc + (menu?.price || 0) * (order._sum.count || 0);
+  }, 0);
 
-  return json({ orders, hourlyOrderCounts });
+  return json({ orders, hourlyOrderCounts, sumOrderCount, sumFee });
 }
 
 export default function Stats() {
-  const { orders, hourlyOrderCounts } = useLoaderData<typeof loader>();
+  const { orders, hourlyOrderCounts, sumOrderCount, sumFee } =
+    useLoaderData<typeof loader>();
 
   const data = orders.map((order) => {
     const menu = menuList.find((menu) => menu.id === order.menu_id);
@@ -34,7 +44,22 @@ export default function Stats() {
     <ScrollArea className={cn("h-full", "p-3", "grow")}>
       <div className={cn("p-3", "flex", "flex-col", "gap-3")}>
         <Hourly data={hourlyOrderCounts} title="時間帯別売上" />
-        <Pie data={data} title="売上比" />
+        <div className={cn("flex", "flex-col", "lg:flex-row", "gap-3")}>
+          <Pie data={data} title="売上比" />
+          <Card
+            className={cn("w-full", "sm:w-1/2", "lg:w-1/3")}
+            decoration="top"
+          >
+            <div className={cn("flex", "flex-col", "gap-1")}>
+              <span className={cn("text-gray-500")}>総売上</span>
+              <span className={cn("text-2xl")}>{sumFee} 円</span>
+            </div>
+            <div className={cn("flex", "flex-col", "gap-1")}>
+              <span className={cn("text-gray-500")}>総注文数</span>
+              <span className={cn("text-2xl")}>{sumOrderCount} 個</span>
+            </div>
+          </Card>
+        </div>
       </div>
     </ScrollArea>
   );
